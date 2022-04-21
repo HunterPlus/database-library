ea10eea4c0562e9e7a31f7853194abcc26c88fad
HunterPlus <zlw08140957@gmail.com>
2022-04-21 08:03:25 +0800

    Create db.c

---
+++ db.c
@@ -0,0 +1,9 @@
+#include "lib.h"
+#include "db.h"
+
+#include <sys/uio.h>		/* struct iovec */
+
+/*
+ * internale index file constants.
+ * These are used to construct record in the index file and data file. */
+ */
