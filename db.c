ad9224180fb6bd8d24dfe8b1f167c4c362922755
HunterPlus <zlw08140957@gmail.com>
2022-04-25 10:30:48 +0800

    Update db.c

--- db.c
+++ db.c
@@ -587,5 +587,28 @@ db_nextrec(DBHANDLE h, char *key)
 	
 	/* we read lock the free list so that we don't read a record
 	   in the middle of its being deleted.		*/
+	if (readw_lock(db->idxfd, FREE_OFF, SEEK_SET, 1) < 0)
+		err_dump("db_nextrec: readw_lock error");
+	
+	do {		/* read next sequential index record */
+		if (_db_readidx(db, 0) < 0) {
+			ptr = NULL;	/* end of index file, EOF */
+			goto doreturn;
+		}
+		/* check if key is all blank (empty record)	*/
+		ptr = db->idxbuf;
+		while ((c = *ptr++) != 0 && c == SPACE)
+			;		/* skip until non byte or nonblank */		
+	} while (c == 0);		/* loop until a nonblank key is found */
+	
+	if (key != NULL)
+		strcpy(key, db->idxbuf);	/* return key */
+	ptr = _db_readdat(db);		/* return pointer to data buffer */
+	db->cnt_nextrec++;
+	
+doreturn：
+	if (un_lock(db->idxfd, FREE_OF, SEEK_SET, 1) < 0)
+		err_dump("db_nextrec: un_lock error");
+	return (ptr);
 }
 
